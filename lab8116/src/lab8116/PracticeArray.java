96a5784609c9ce82b1b1fec61027a12b457cd5af
purnimaV07 <purn0005@algonquinlive.com>
2024-12-04 13:06:04 -0500

    New Project Hello World with git I think I am 'git-ing' it (groan) Signed-off-by: Purnima Purnima <purn0005@algonquinlive.com>

---
+++ lab8116/src/lab8116/PracticeArray.java
@@ -0,0 +1,19 @@
+package lab8116;
+
+public class PracticeArray {
+
+	public static void main(String[] args) {
+		 
+		 //char[] scores = {'a','b','c','d','e'};
+	        //System.out.println( scores[1]); 
+	        
+	       // System.out.println( scores.length); 
+	        //int[] scores =new int[5];
+	        //scores.length= 10;
+	        //String word = "Cow";
+	        //System.out.println( word.length());
+		double[] prices;
+		prices=new double[5];
+		}
+
+}
